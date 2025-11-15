d7daacfe23107a62dd14593ab8c218f87a4d1df4
HacThienCau <22521641@gm.uit.edu.vn>
2025-11-15 11:32:21 +0700

    feat: add cart page

---
+++ src/components/Cart/RemoveDialog.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { ChevronLeftIcon, Trash } from "lucide-react";
+import { removeFromCart } from "@/service/cartService";
+import { useToast } from "@/contexts/ToastContext";
+
+function RemoveDialog({ cp }) {
+  const { success, error } = useToast();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleDelete = async () => {
+    setLoading(true);
+    const res = await removeFromCart(cp._id);
+    if (res.success) success(res.message);
+    else error(res.error);
+    setLoading(false);
+    setOpen(false);
+  };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          className="text-red-500"
+          variant="ghost"
+          title="Xóa khỏi giỏ hàng"
+        >
+          <Trash />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Xóa khỏi giỏ hàng</DialogTitle>
+          <DialogDescription>
+            Bạn có chắc chắn muốn xóa sản phẩm này khỏi giỏ hàng? Hành động này
+            sẽ không thể hoàn tác!
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex items-center justify-end">
+          <DialogClose asChild>
+            <Button variant="outline">
+              <ChevronLeftIcon />
+              Hủy
+            </Button>
+          </DialogClose>
+          <Button
+            disabled={loading}
+            variant="destructive"
+            onClick={handleDelete}
+          >
+            <Trash />
+            <p>Xóa khỏi danh sách</p>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default RemoveDialog;
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import CartList from "@/components/Cart/CartList";
+import Header from "@/components/Header";
+import React from "react";
 
 function Cart() {
   return (
-    <div>Cart</div>
-  )
+    <div className="flex flex-col w-full min-h-screen">
+      <Header />
+      <div
+        className="flex-1 w-full max-w-7xl px-4 sm:px-6 md:px-8 lg:px-10 
+        h-full flex flex-col
+        pt-6 sm:pt-8 md:pt-10 
+        mx-auto 
+        gap-4 sm:gap-6 
+        bg-white/95 backdrop-blur-sm
+        container"
+      >
+        <h2 className="font-semibold text-2xl">Giỏ hàng</h2>
+        <CartList />
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
